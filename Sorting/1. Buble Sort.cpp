44824ef4c66ebf5a392e208402e8685d51df222d
deepanker_rawat <83623608+Deepanker-rawat@users.noreply.github.com>
2026-01-11 19:12:27 +0530

    Create 1. Buble Sort.cpp

---
+++ Sorting/1. Buble Sort.cpp
@@ -0,0 +1,10 @@
+void bubbleSort(vector<int>& arr) {
+        int n = arr.size();
+        for(int i=0; i<n; i++){
+            for(int j=1; j<n-i; j++){
+                if(arr[j-1]>arr[j]){
+                    swap(arr[j-1], arr[j]);
+                }
+            }
+        }
+    }
