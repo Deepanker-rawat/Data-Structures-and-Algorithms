94b8ffb3639cecd3e73758c4945502008c3ca3ac
deepanker_rawat <83623608+Deepanker-rawat@users.noreply.github.com>
2026-01-12 23:54:19 +0530

    Create Insertion sort.cpp

---
+++ Sorting/Insertion sort.cpp
@@ -0,0 +1,15 @@
+ void insertionSort(vector<int>& arr) {
+        
+        for(int i=1; i<arr.size(); i++){
+            int key = arr[i];
+            int j = i-1;
+            
+            while(j>=0 and arr[j]>key){
+                arr[j+1] = arr[j];
+                j--;
+            }
+            
+            arr[j+1] = key;
+        }
+        
+    }
