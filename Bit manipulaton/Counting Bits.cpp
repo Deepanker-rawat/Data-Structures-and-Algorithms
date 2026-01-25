17567ffd0cebec2cbd1d31551558d1b049acc397
deepanker_rawat <83623608+Deepanker-rawat@users.noreply.github.com>
2026-01-25 17:34:09 +0530

    Create Counting Bits.cpp

---
+++ Bit manipulaton/Counting Bits.cpp
@@ -0,0 +1,14 @@
+// Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
+// Leetcode : https://leetcode.com/problems/counting-bits
+
+vector<int> countBits(int n) {
+        vector<int> ans(n+1, 0);
+
+        for(int i=1;i<=n; i++){
+           ans[i] = ans[i & (i - 1)] + 1;
+        }
+
+        return ans;
+
+
+}
